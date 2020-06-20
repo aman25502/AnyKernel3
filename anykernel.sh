558f8da4ff668dacc0d275ba850108f7e496397f
Dhruv <dhruvgera61@gmail.com>
2020-06-20 14:06:59 +0530

    anykernel: Adapt for Rockstar
    Signed-off-by: Dhruv <dhruvgera61@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,14 +4,13 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=EverestKernel by ProtoChuz @ GitHub
+kernel.string=Rockstar Kernel by Dhruv
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=santoni
-device.name2=lavender
 supported.versions=
 supported.patchlevels=
 '; } # end properties
