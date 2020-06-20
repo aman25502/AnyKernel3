f7d6a9a23912004567ddfb5f99e9f344257dafab
aman25502 <65896145+aman25502@users.noreply.github.com>
2020-06-20 14:37:50 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Rockstar Kernel by Dhruv
+kernel.string=Testing kernel by ahoy
 do.devicecheck=1
 do.modules=0
 do.systemless=1
